2203fc51545d708780e38914f0ec83c4c4d98e06
eeKunev <SazDarMaa@gmail.com>
2021-05-06 19:56:49 +0300

    Create User.java

---
+++ src/ChatBot/User.java
@@ -0,0 +1,20 @@
+package ChatBot;
+
+import java.awt.*;
+
+public abstract class User {
+
+    protected MessageMediator mediator;
+    protected String name;
+
+    public User(MessageMediator mediator,String name)
+    {
+        this.mediator = mediator;
+        this.name = name;
+    }
+
+    public abstract void sendMessage(String message);
+    public abstract void receiveMessage(String message);
+    public abstract void leave();
+
+}
